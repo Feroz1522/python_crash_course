b1681a6c4191d4f78cd1bd7648d5120e93d78fea
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-06-14 11:39:23 +0530

    Data types & varibles

---
+++ Chapter - 2/apostrophe.py
@@ -0,0 +1,2 @@
+message = " i'm here to checking apostrophe error by myself lol! :) "
+print(message)
---
+++ Chapter - 2/restr.py
@@ -0,0 +1,4 @@
+get_usr = "hi i am zoref "
+print(get_usr)
+get_usr =  " i am feroz "
+print(get_usr)
