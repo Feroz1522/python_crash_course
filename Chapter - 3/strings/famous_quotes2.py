c3acc9cfe8e7b8da5d42b59c171611301804640c
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-06-14 11:36:40 +0530

    depth concepts using strings and variables

---
+++ Chapter - 3/strings/famous_quotes2.py
@@ -0,0 +1,4 @@
+famous_name = "\t Feroz Khan "
+message = 'said once ,"Don\'t Greedy on others things \n\t Help others and make changes in others life ."' 
+print(famous_name + message)
+
---
+++ Chapter - 3/strings/fav_num.py
@@ -0,0 +1,4 @@
+#this code is writtened for using string and number by concatination
+favourite_number = 5
+print(" hi guyz my Favourite number is " + str(favourite_number) )
+print(f" a friendly remainder once again '{favourite_number}' ")
