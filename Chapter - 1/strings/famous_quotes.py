669f0f17fc5a43dd5adddc9e3f0fec4b5872487a
ZOREF <102370932+Feroz1522@users.noreply.github.com>
2023-06-14 11:27:32 +0530

    Chapter - 1

---
+++ Chapter - 1/strings/famous_quotes.py
@@ -0,0 +1,1 @@
+print('\t Taslim Nisha once said , " Help others and share your stuffs \n\t don\'t greedy on other things."')  
---
+++ Chapter - 1/strings/personal_message.py
@@ -0,0 +1,4 @@
+usr_name = " Feroz"
+print(f"hello {usr_name} would you like to learn some python stuffs ")
+print("Here " + usr_name + " lets start to learn ")
+
